b4f0cc892a39c6f028f6af60630e04d4ac89db0b
Israel Paz <israel_paz@msn.com>
2022-07-13 18:55:29 -0500

    Clase 11 Version 2: planificacion y configuracion inicial

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,6 +20,10 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-
-Route::apiResource('v1/posts', \App\Http\Controllers\Api\V1\PostController::class)
+//V1
+Route::apiResource('v1/posts', PostV1::class)
 ->only(['index', 'show', 'destroy']);
+
+//V2
+Route::apiResource('v2/posts', PostV2::class)
+->only(['index', 'show']);
