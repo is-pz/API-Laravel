25683105f7626db748936659752305a04f982e20
Israel Paz <israel_paz@msn.com>
2022-07-13 19:26:52 -0500

    Clase 15 Autenticacion

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,5 @@ Route::apiResource('v1/posts', PostV1::class)
 Route::apiResource('v2/posts', PostV2::class)
     ->only(['index', 'show'])
     ->middleware('auth:sanctum');
+
+Route::post('login', [\App\Http\Controllers\Api\LoginController::class, 'login']);
